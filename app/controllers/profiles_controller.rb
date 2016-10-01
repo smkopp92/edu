5461a53f34874baaad51f073cf70e1ce078f0412
Sebastian Kopp <smkopp92@gmail.com>
2016-10-01 02:09:15 -0400

    create user profile:

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,4 +2,17 @@ class ProfilesController < ApplicationController
   def new
     @profile = Profile.new
   end
+
+  def create
+    @profile = Profile.new(profile_params)
+    @profile.user = current_user
+    @profile.save
+    redirect_to '/'
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:sat, :gpa, :social, :athletics)
+  end
 end
