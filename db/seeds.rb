43f7beabe43cbb37c3d896b55e9b506f26d7dbf2
Kevin Pasquarella <pasquarella4292@gmail.com>
2016-11-30 22:15:40 -0500

    api tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,4 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 College.create(name: "Kevin College", location: "Your mom", sat: 20000, gpa: 0.000001, social: 1, athletics: 123456)
 College.create(name: "Sebastian College", location: "Your mom", sat: 1, gpa: 1, social: 1, athletics: 1)
+College.create(name: "Boston College", location: "New England", sat: 2045, gpa: 3.72, social: 8, athletics: 7)
